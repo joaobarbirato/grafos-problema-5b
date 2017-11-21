8700aa59e7cd6c8919add8bac43a1749c3cd728d
joao barbirato <joaobarbirato@gmail.com>
2017-11-20 23:53:27 -0200

    Criado o dataset

---
+++ src/dataset.py
@@ -0,0 +1,14 @@
+import networkx as nx
+
+# Creating nodes
+G = nx.Graph()
+for i in range(0,20):
+    G.add_node(str(i))
+
+# Creating edges
+edgeFile = open('./data/edges.txt', 'rt')
+edges = edgeFile.readlines()
+for i in range(0,20):
+    G.add_edge((edges[i].split(' '))[0], (edges[i].split(' '))[1])
+
+edgeFile.close()
